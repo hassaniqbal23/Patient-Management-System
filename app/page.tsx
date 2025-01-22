95224052fad3d8f014e97d58d3fa961a701b4485
hassaniqbal23 <hassaniqbalkiu@gmail.com>
2025-01-22 15:38:48 +0500

    create admin access modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import {PatientForm} from "@/components/forms/PatientForm"
+import PassKeyModal from "@/components/PassKeyModal"
 import Image from "next/image"
 import Link from "next/link"
 
-export default function Home() {
+export default function Home({searchParams}: SearchParamProps) {
+  const isAdmin = searchParams.admin === "true"
 return (
   <div className="flex h-screen max-h-screen text-gray-300">
+    {isAdmin && <PassKeyModal/>}
     {/* TODO: OTP VARIFICATION | PASSKEYMODAL */}
     <section className="remove-scrollbar container my-auto">
      <div className="sub-container ">
