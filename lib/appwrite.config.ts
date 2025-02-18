914852799c70536a941f638fa10618bb7d16ca8a
hassaniqbal23 <hassaniqbalkiu@gmail.com>
2025-02-18 15:59:15 +0500

    add messages in ap

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,5 +1,4 @@
 import * as sdk from "node-appwrite";
-
 export const {
   NEXT_PUBLIC_ENDPOINT: ENDPOINT,
   PROJECT_ID,
@@ -22,3 +21,4 @@ client.setEndpoint(ENDPOINT).setProject(PROJECT_ID).setKey(API_KEY);
 export const databases = new sdk.Databases(client);
 export const storage = new sdk.Storage(client);
 export const users = new sdk.Users(client);
+export const messaging = new sdk.Messaging(client);
